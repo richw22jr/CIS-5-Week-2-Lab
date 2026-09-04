e0916ba135c9e29bdd0b88723d9fb84922eef380
richw22jr <richw22@hotmail.com>
2026-09-04 01:19:33 +0000

    Replaced TODOs with my labeled lines

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-// Lab 2 — Your Name
+// Lab 2 — Richard Webster
 // CIS 5 Week 02 · Program structure
 
 int main() {
   // TODO: replace TODOs with YOUR labeled lines (5–6 total)
-  std::cout << "Name: TODO\n";
-  std::cout << "Interest: TODO\n";
-  std::cout << "CIS 5 goal: TODO\n";
-  std::cout << "Tool path: TODO\n";
-  std::cout << "Fun fact: TODO\n";
+  std::cout << "Name: Richard\n";
+  std::cout << "Interest: School\n";
+  std::cout << "CIS 5 goal: Pass the class\n";
+  std::cout << "Tool path: GitHub Codespaces \n";
+  std::cout << "Fun fact: I hate computers\n";
   return 0;
 }
